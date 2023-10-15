e2d5409be70446a552d45b3fcc391e4a1335060b
EC2 Default User <ec2-user@ip-172-31-0-97.ap-northeast-1.compute.internal>
2023-10-15 13:57:56 +0000

    add controller&view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  
+  devise_for :admins, skip: [:registrations, :passwords],  controllers: {
+    sessions: "admins/sessions"
+  }
+  
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "publics/registrations",
+    sessions: "publics/sessions"
+  }
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
