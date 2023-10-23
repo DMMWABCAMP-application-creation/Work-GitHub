54025dcee2e882452a62eb6ff1081f2778a113df
EC2 Default User <ec2-user@ip-172-31-0-97.ap-northeast-1.compute.internal>
2023-10-23 04:59:15 +0000

    新規会員登録画面作成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   # before_action :configure_account_update_params, only: [:update]
-  before_action :configure_sign_in_params, only: [:create]
+  #before_action :configure_sign_in_params, only: [:create]
+  
+  
+  
+  def after_sign_up_path_for(resource)
+    my_page_path
+  end
   
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  #GET /resource/sign_up
+  #def new
+  #  @customer = Customer.new
+  #end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  #def create
+  #  @customer = Customer.new
+  #end
 
   # GET /resource/edit
   # def edit
@@ -41,12 +48,19 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-  
-  def configure_sign_in_params
-   devise_parameter_sanitizer.permit(:sign_in, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana,
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, 
     :postal_code, :address, :telephone_number])
   end
+  
+  
+  #def configure_sign_in_params
+  # devise_parameter_sanitizer.permit(:sign_in, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana,
+  #  :postal_code, :address, :telephone_number])
+  #end
 
+  
   # If you have extra params to permit, append them to the sanitizer.
   #def configure_sign_up_params
   #  devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana,
